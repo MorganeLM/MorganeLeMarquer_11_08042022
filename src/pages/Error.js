48e2019838efad529222d894ee8a8e46ded9d798
MorganeLM <morganelemarquer@lilo.org>
2022-04-29 16:29:58 +0200

    style 404 page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
+import '../styles/Error.css'
+
 function Error() {
-  //redirect to previous page
-  //navigate(-1);
     return (
-      <div className="Error">
+      <div className="error">
         <h1>404</h1>
+        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <Link to='/'>Retourner sur la page d'accueil</Link>
       </div>
     );
   }
